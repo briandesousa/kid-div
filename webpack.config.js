e5400c96e98c4deab0014ed7291c5acb4df39ed4
Brian De Sousa <briandesousa1@gmail.com>
2019-09-21 00:14:39 -0400

    enable source-map generation with webpack to fix error where source maps for two NPM packages could not be downloaded in the browser (console error)

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
